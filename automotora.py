f8e5161965304b270c571866670320e003a69c50
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2021-08-26 18:10:50 -0400

    se creó el constructor y la funcion agregar auto

---
+++ automotora.py
@@ -0,0 +1,10 @@
+class Automotora:
+    def Automotora(self,nombre):
+        self.listaAutos=[]
+        self.nombre=nombre
+
+    def agregarAuto(dueño,patente):
+        listaAutos.append(Auto(dueño,patente))
+
+
+    
